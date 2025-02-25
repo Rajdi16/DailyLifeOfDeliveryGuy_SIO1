00b8361fb68942da5cd2d8acdac95e7012d2de42
celinaninadr <dricicelina@icloud.com>
2025-02-25 16:51:05 +0100

    nouveau import
    remplacement des import

--- Assets/Script/Chrono.cs
+++ Assets/Script/Chrono.cs
@@ -1,5 +1,10 @@
-using System.Threading;
+using JetBrains.Annotations;
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
 
 public static void Main()
 {
