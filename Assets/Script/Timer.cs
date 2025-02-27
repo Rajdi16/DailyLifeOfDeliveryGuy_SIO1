5b66174acb1879eb81b9d29b96de93f5d62dc7f3
valentin-binaku <v.binaku05@gmail.com>
2025-02-27 09:20:13 +0100

    [fix] Timer

--- Assets/Script/Timer.cs
+++ Assets/Script/Timer.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Timer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField] TextMeshProUGUI timerText;
+    float elapsedTime;
+    
     void Update()
     {
-        
+        elapsedTime += Time.deltaTime;
+        timerText.text = elapsedTime.ToString();
     }
 }
