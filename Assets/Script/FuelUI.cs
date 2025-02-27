42cfa611a1a5d514df47ad285db2b877724ed5c1
MarvinFm <marvin.fm54@gmail.com>
2025-02-27 11:49:19 +0100

    Commentaires dans FuelUI

--- Assets/Script/FuelUI.cs
+++ Assets/Script/FuelUI.cs
@@ -4,20 +4,21 @@ using UnityEngine.UI;
 
 public class FuelUI : MonoBehaviour
 {
-    public TextMeshProUGUI FuelOnJauge;
-    public Image Pointe;
-    public FuelConsumption fuelConsumption;
+    public TextMeshProUGUI FuelOnJauge; // Texte sur la jauge
+    public Image Pointe; // Pointe de la jauge
+    public FuelConsumption fuelConsumption; // Fuel restant
 
     private void Start()
     {
         FuelOnJauge.text = "000"; // Par défaut
+        fuelConsumption = GetComponent<FuelConsumption>();
     }
     private void Update()
     {
-        FuelOnJauge.text = Mathf.Max(fuelConsumption.fuel, 0).ToString("F2");
+        FuelOnJauge.text = Mathf.Max(fuelConsumption.fuel, 0).ToString("F2"); // Affichage du fuel restant
 
         float rotationAngle = Mathf.Lerp(0f, 90f, fuelConsumption.fuel / 100f); // Il faudra régler pour que l'éguille s'affiche bien
 
-        Pointe.transform.rotation = Quaternion.Euler(0, 0, -rotationAngle);
+        Pointe.transform.rotation = Quaternion.Euler(0, 0, -rotationAngle); // Rotation de la pointe
     }
 }
