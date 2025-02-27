e13ae521a2ef37bcedf587fea0d8778a22c8ea99
MarvinFm <marvin.fm54@gmail.com>
2025-02-27 10:53:52 +0100

    Update FuelUI.cs

--- Assets/Script/FuelUI.cs
+++ Assets/Script/FuelUI.cs
@@ -1,13 +1,19 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class FuelUI : MonoBehaviour
 {
-    public Text fuelText;
+    public TextMeshProUGUI FuelOnJauge;
+    public Image Pointe;
     public FuelConsumption fuelConsumption;
 
     private void Update()
     {
-        fuelText.text = "Fuel: " + Mathf.Max(fuelConsumption.fuel, 0).ToString("F2");
+        FuelOnJauge.text = Mathf.Max(fuelConsumption.fuel, 0).ToString("F2");
+
+        float rotationAngle = Mathf.Lerp(0f, 90f, fuelConsumption.fuel / 100f); // Il faudra régler pour que l'éguille s'affiche bien
+
+        Pointe.transform.rotation = Quaternion.Euler(0, 0, -rotationAngle);
     }
 }
