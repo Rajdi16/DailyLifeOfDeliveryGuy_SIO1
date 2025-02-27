42674cfc09ac1c6721463541da7e9c8c2c9b2c09
sael-sellesfrancesconi <sael.sellesfrancesconi@outlook.com>
2025-02-27 11:52:05 +0100

    ajout de commentaires

--- Assets/Script/FuelConsumption.cs
+++ Assets/Script/FuelConsumption.cs
@@ -5,40 +5,48 @@ using UnityEngine;
 
 public class FuelConsumption
 {
+    // Quantité initiale de carburant
     public float fuel = 100000;
+    // Taux de consommation de carburant en courant
     public float fuelConsumptionRateRunning = 3;
+    // Taux de consommation de carburant en marchant
     public float fuelConsumptionRateWalking = 1;
 
+    // Référence au mouvement de la voiture
     private LivraisonCarMovement carMovement;
 
     private void Start()
     {
+        // Obtient le composant LivraisonCarMovement attaché à l'objet
         carMovement = GetComponent<LivraisonCarMovement>();
     }
 
     private void Update()
     {
-        
+        // Vérifie si il reste du carburant
         if (fuel > 0)
         {
+            // Si la voiture est en train de courir, consomme du carburant à un taux plus élevé
             if (carMovement.isRunning)
             {
                 fuel -= fuelConsumptionRateRunning * Time.deltaTime;
             }
+            // Sinon, consomme du carburant à un taux plus faible
             else
             {
                 fuel -= fuelConsumptionRateWalking * Time.deltaTime;
             }
 
+            // Si le carburant est épuisé, arrête la voiture
             if (fuel <= 0)
             {
                 fuel = 0;
-                
                 carMovement.speed = 0;
             }
         }
     }
 
+    // Méthode pour ajouter du carburant
     private void addFuel(int quantite)
     {
         fuel += quantite;
--- Assets/Script/LivraisonCarMovement.cs
+++ Assets/Script/LivraisonCarMovement.cs
@@ -5,29 +5,31 @@ using UnityEngine;
 
 public class LivraisonCarMovement : MonoBehaviour
 {
-    public float speed;
-    [SerializeField] private float runSpeed;
-    [SerializeField] private float walkSpeed;
-    [SerializeField] private float horizontalSpeed;
+    public float speed; // Vitesse actuelle de la voiture
+    [SerializeField] private float runSpeed; // Vitesse de course
+    [SerializeField] private float walkSpeed; // Vitesse de marche
+    [SerializeField] private float horizontalSpeed; // Vitesse de rotation horizontale
 
-    public bool isRunning = false;
-    private Vector2 input;
-    [SerializeField] private Rigidbody rigidbody;
+    public bool isRunning = false; // Indique si la voiture est en train de courir
+    private Vector2 input; // Stocke les entrées de l'utilisateur
+    [SerializeField] private Rigidbody rigidbody; // Référence au Rigidbody de la voiture
 
-    private bool isGrounded = true;
+    private bool isGrounded = true; // Indique si la voiture est au sol
 
-    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject player; // Référence au joueur
 
-    private FuelConsumption fuelConsumption;
+    private FuelConsumption fuelConsumption; // Référence à la consommation de carburant
 
     private void Start()
     {
+        // Initialisation des composants
         rigidbody = GetComponent<Rigidbody>();
         fuelConsumption = GetComponent<FuelConsumption>();
     }
 
     private void Update()
     {
+        // Vérifie si la touche LeftShift est enfoncée pour déterminer la vitesse
         if (Input.GetKey(KeyCode.LeftShift))
         {
             speed = runSpeed;
@@ -39,14 +41,17 @@ public class LivraisonCarMovement : MonoBehaviour
             isRunning = false;
         }
 
+        // Vérifie si le bouton de la souris est enfoncé pour instancier le joueur et détruire la voiture
         if (Input.GetKeyDown(KeyCode.Mouse0) && Time.deltaTime != 0)
         {
             Instantiate(player, gameObject.transform.position, gameObject.transform.rotation);
             Destroy(gameObject);
         }
 
+        // Récupère les entrées de l'utilisateur pour le mouvement
         input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
+        // Vérifie si la voiture est au sol et si la touche Espace est enfoncée pour désactiver l'état au sol
         if (isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
             isGrounded = false;
@@ -55,15 +60,18 @@ public class LivraisonCarMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        // Applique la rotation en fonction des entrées horizontales
         Vector3 desireRotation = new Vector3(0, input.x * horizontalSpeed * Time.deltaTime, 0);
         rigidbody.MoveRotation(rigidbody.rotation * Quaternion.Euler(desireRotation));
 
+        // Applique le mouvement en fonction des entrées verticales
         Vector3 movement = new Vector3(transform.forward.x * input.y * speed * Time.deltaTime, 0, transform.forward.z * input.y * speed * Time.deltaTime);
         rigidbody.MovePosition(transform.position + movement);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        // Vérifie si la collision est avec le sol pour activer l'état au sol
         if (collision.gameObject.layer == 3)
         {
             isGrounded = true;
