bda1bab0abfcc6fb6be54b588ccea4162dde80d4
celinaninadr <dricicelina@icloud.com>
2025-02-27 10:42:57 +0100

    Ajout des commentaires

--- Assets/Script/Timer.cs
+++ Assets/Script/Timer.cs
@@ -1,19 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using TMPro;
+using UnityEngine; // to use the functionality of Unity
+using TMPro; // TMPro = TextMesh Pro , for improved text in unity
 
-public class Timer : MonoBehaviour
+public class Timer : MonoBehaviour //Provides the fonctionnality to control game objetcs.
 {
-    [SerializeField] TextMeshProUGUI timerText;
-    float elapsedTime;
-    
+    [SerializeField] TextMeshProUGUI timerText; 
+    float elapsedTime; // Creation of variable to store time from the beginning of the game
     void Update()
     {
-        elapsedTime += Time.deltaTime;
-        timerText.text = elapsedTime.ToString();
-        int minutes = Mathf.FloorToInt(elapsedTime / 60);
-        int seconds = Mathf.FloorToInt(elapsedTime % 60);
-        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        elapsedTime += Time.deltaTime; //Time increment
+        timerText.text = elapsedTime.ToString(); //display time as a character string  
+        //Below, it calculates the time
+        int minutes = Mathf.FloorToInt(elapsedTime / 60); // Convert time to minute 
+        //Mathf.FloorToint is used to round a decimal number to the nearest integer
+        int seconds = Mathf.FloorToInt(elapsedTime % 60); //Divide the time by 60, for the seconds 
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds); //To display the minutes and seconds in a format (MM:SS)
     }
 }
